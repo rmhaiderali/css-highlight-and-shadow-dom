6258fe798d1f1e319a0d4173468082d8d0369743
Haider Ali <rmha62@gmail.com>
2025-09-14 16:24:09 +0500

    Rename project to css-highlight-and-shadow-dom

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/css-highlight-pseudo-elements-inheritance-and-shadow-dom",
+  base: "/css-highlight-and-shadow-dom",
 })
